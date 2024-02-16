073803989ffd34b0d62c455c27edb8e9980f5c1f
sommy-josh <chisomzzy1@>
2024-02-16 02:08:44 +0100

    another change

--- user_Auth/serializers.py
+++ user_Auth/serializers.py
@@ -5,13 +5,13 @@ from rest_framework.validators import ValidationError
 class UserSerializer(serializers.ModelSerializer):
     username=serializers.CharField(max_length=20)
     email = serializers.EmailField(max_length=255)  # Assuming a reasonable maximum length
-
     password=serializers.CharField(min_length=8, write_only=True)
     class Meta:
         model=User
         fields=['username','email','password',]
 
     def validate(self, attrs):
+    
         email_exists=User.objects.filter(email=attrs['email']).exists()
         if email_exists:
             raise ValidationError("Email has already been used")
